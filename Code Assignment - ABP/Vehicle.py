0173de12794ab1a6ce9cc3c4d45aeca2937c65ff
Ayush Saxena <ayushsaxena312@outlook.com>
2020-09-05 11:23:37 +0530

    import file

---
+++ Code Assignment - ABP/Vehicle.py
@@ -0,0 +1,5 @@
+class vehicle:
+    def __init__(self, vehiclet_ype, license_plate, company):
+        self.vehiclet_ype = vehiclet_ype
+        self.license_plate = license_plate
+        self.company = company
